4ed0fee1ca04356b757e09fd1072d3216612ff68
dea846 <73856838+dea846@users.noreply.github.com>
2021-02-03 21:04:45 -0600

    Add files via upload

--- CCAS_Motion_Sim.m
+++ CCAS_Motion_Sim.m
@@ -7,25 +7,108 @@ PSDx = m.PSDx;freqx = m.freqx';
 PSDy = m.PSDy;freqy = m.freqy';
 PSDz = m.PSDz;freqz = m.freqz';
 %Inverse FFT from Freq domain to Time Domain
-min=30; %Length of simulated signal
-[DispX_sig,t_sig]=invFFT(min,PSDx,Fs,freqx);
-[DispY_sig,~]=invFFT(min,PSDy,Fs,freqy);
-[DispZ_sig,~]=invFFT(min,PSDz,Fs,freqz);
+minute=30*60/60; %Length of simulated signal
+[DispX_sig,t_sig]=invFFT(minute,PSDx,Fs,freqx);
+[DispY_sig,~]=invFFT(minute,PSDy,Fs,freqy);
+[DispZ_sig,~]=invFFT(minute,PSDz,Fs,freqz);
 
 %Spot Motion Sim
-cordx=0;cordy=0;
-for i=1:length(DispX_sig)
-    newX=cordx(i)+DispX_sig(i);
-    newY=cordy(i)+DispY_sig(i);
-    cordx=[cordx; newX];cordy=[cordy; newY];
+sr=0.02; %sec, sampling rate of accelerometer, 20 ms
+exposure=1; %seconds
+cordx=DispX_sig/125e-6;cordy=DispY_sig/125e-6; %in arcsec, starting spot value
+cordx=reshape(cordx,1/sr*exposure,minute*60/exposure);cordy=reshape(cordy,1/sr*exposure,minute*60/exposure);
+figure()
+hold on;grid on;
+plot(cordx,cordy,"o")
+xlabel("X Cordinates (arcsec)")
+ylabel("Y Cordinates (arsec)")
+title("X-Y Motion of Dot")
+
+%Radial Distance Code
+%pixel=0.044; %arcsec per pixel size
+%r=sqrt(cordx.^2+cordy.^2); %center is 0,0
+%f=histogram(r);
+%y=0:pixel:1.1*max(r); %bin sizes
+%f.BinEdges=y;
+%hold on;
+%mu=mean(r); %average of radii
+%sigma=std(r); %standard deviation of radii
+%func=exp(-(y-mu).^2./(2*sigma^2))*max(f.Values);%./(sigma*sqrt(2*pi));
+%plot(y,func,'LineWidth',1.5)
+%xlabel("Radii Distance (arcsec)")
+%ylabel("Bin Count")
+%title("Histogram of Spot Motion Radii")
+
+%Image Distortion Animation
+pixel=0.044; %arcsec, 1 pixel size
+sigma=pixel+pixel*rand(1);%random width of bell from 1 to 2 pixels
+grid_s=80; %50 square pixel grid
+[X,Y]=meshgrid(-grid_s/2*pixel:pixel:grid_s/2*pixel, -grid_s/2*pixel:pixel:grid_s/2*pixel);
+A=1; %Amplitude
+B=size(cordx); %Size of reshaped cordx matrix
+filename = '60secTestNoisy.gif';
+f=figure(2);
+axis tight manual
+Z=zeros(length(X),length(Y),B(2)); %meshgrid zero
+for i=1:B(2)
+    for j=1:B(1)
+        xo=cordx(j,i);yo=cordy(j,i);%amplitude
+        Gaus_2d=A*exp(-(((X-xo).^2)/(2*sigma^2)+((Y-yo).^2)/(2*sigma^2)));
+        Z(:,:,i)=Z(:,:,i)+Gaus_2d;
+    end
+    %Intesnity-weighted centroid of image
+    centroidx(i)=sum(Z(:,:,i).*X)/sum(Z(:,:,i));
+    centroidy(i)=sum(Z(:,:,i).*Y)/sum(Z(:,:,i));
+    if i==1
+        g=pcolor(X,Y,Z(:,:,i));
+        title("Image Animation")
+        xlabel("X cord (arcsec)")
+        ylabel("Y cord (arcsec)")
+        set(g, "EdgeColor", "none")
+        drawnow();
+        frame = getframe(f); 
+        im = frame2im(frame); 
+        [imind,cm] = rgb2ind(im,256);
+        imwrite(imind,cm,filename,'gif', 'Loopcount',inf,'DelayTime',0); 
+    else 
+        set(g, "CData", Z(:,:,i));
+        set(g, "EdgeColor", "none")
+        drawnow();
+        frame = getframe(f); 
+        im = frame2im(frame); 
+        [imind,cm] = rgb2ind(im,256);
+        imwrite(imind,cm,filename,'gif','WriteMode','append','DelayTime',0);
+    end
 end
 figure()
 hold on;grid on;
-plot(cordx,cordy)
-plot(cordx(1),cordy(1),"ro")
-xlabel("X Cordinates (m)")
-ylabel("Y Cordinates (m)")
-title("X-Y Motion of Dot")
+plot(centroidx,centroidy,"o")
+xlabel("Xcord (arcsec)")
+ylabel("Ycord (arcesec)")
+title("Motion of Image Centroid")
+%Spot Motion Animation Code
+% h = figure;
+% axis tight manual % this ensures that getframe() returns a consistent size
+% filename = '1secTest.gif';
+% for i=1:length(cordx) %length of data is same for all
+%     plot(cordx(i),cordy(i),"o")
+%     title("X-Y Animation of Dot")
+%     xlabel("X Cordinates (arcsec)")
+%     ylabel("Y Cordinates (arsec)")
+%     xlim([1.1*min(cordx) 1.1*max(cordx)])
+%     ylim([1.1*min(cordy) 1.1*max(cordy)])
+%     drawnow 
+%     % Capture the plot as an image 
+%     frame = getframe(h); 
+%     im = frame2im(frame); 
+%     [imind,cm] = rgb2ind(im,256); 
+%     % Write to the GIF File 
+%     if i == 1 
+%         imwrite(imind,cm,filename,'gif', 'Loopcount',inf,'DelayTime',0); 
+%     else 
+%         imwrite(imind,cm,filename,'gif','WriteMode','append','DelayTime',0); 
+%     end
+% end
 
 %%% Plotting Section %%%
 % figure()
