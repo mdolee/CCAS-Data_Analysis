f81fc356b5d62ef19febc49cfbeec182ce1858d4
dea846 <73856838+dea846@users.noreply.github.com>
2021-05-10 14:36:47 -0500

    Update CCAS_Motion_Sim.m

--- CCAS_Motion_Sim.m
+++ CCAS_Motion_Sim.m
@@ -1,6 +1,11 @@
 clc;clear; close all;
 format long g 
 tic
+% Report of Findings
+% Fix 30 min duration to carve time to smaller/larger time
+% Pick rndom points until signal adds up to lenght of signal
+% Record animation length for only first minute
+
 %Load in PSD data files
 m = matfile('PSD.mat');Fs=m.Fs;
 PSDx = m.PSDx;freqx = m.freqx';
@@ -12,80 +17,79 @@ minute=30*60/60; %Length of simulated signal
 [DispY_sig,~]=invFFT(minute,PSDy,Fs,freqy);
 [DispZ_sig,~]=invFFT(minute,PSDz,Fs,freqz);
 
+%Coordinate System Transformation
+%(raw coord system data is in x,y,z, roll 55 deg CW)
+theta=55; %deg
+DispXnew=DispX_sig;                                  %[1    0      0];
+DispYnew=DispY_sig*cosd(theta)-DispZ_sig*sin(theta); %[0 cos(t) -sin(t)]
+DispZnew=DispY_sig*sind(theta)+DispZ_sig*cosd(theta);%[0 sin(t) cos(t)]
+%Spot Motion now in (xnew,znew) plane, ynew affects size of dot
+
 %Spot Motion Sim
 sr=0.02; %sec, sampling rate of accelerometer, 20 ms
-exposure=1; %seconds
-cordx=DispX_sig/125e-6;cordy=DispY_sig/125e-6; %in arcsec, starting spot value
-cordx=reshape(cordx,1/sr*exposure,minute*60/exposure);cordy=reshape(cordy,1/sr*exposure,minute*60/exposure);
-figure()
-hold on;grid on;
-plot(cordx,cordy,"o")
-xlabel("X Cordinates (arcsec)")
-ylabel("Y Cordinates (arsec)")
-title("X-Y Motion of Dot")
+exposure=1/10; %seconds
+cordx=DispXnew;cordz=DispZnew; %arcsec, starting spot value
+cordx=reshape(cordx,1/sr*exposure,minute*60/exposure);cordz=reshape(cordz,1/sr*exposure,minute*60/exposure);
+dy=(DispYnew-mean(DispYnew))/125e-6; %arcsec, converting to arcsec 
 
-%Radial Distance Code
-%pixel=0.044; %arcsec per pixel size
-%r=sqrt(cordx.^2+cordy.^2); %center is 0,0
-%f=histogram(r);
-%y=0:pixel:1.1*max(r); %bin sizes
-%f.BinEdges=y;
-%hold on;
-%mu=mean(r); %average of radii
-%sigma=std(r); %standard deviation of radii
-%func=exp(-(y-mu).^2./(2*sigma^2))*max(f.Values);%./(sigma*sqrt(2*pi));
-%plot(y,func,'LineWidth',1.5)
-%xlabel("Radii Distance (arcsec)")
-%ylabel("Bin Count")
-%title("Histogram of Spot Motion Radii")
+figure(1)
+hold on;grid on;
+plot(cordx,cordz,"o")
+xlabel("X Cordinates (arcsec)")
+ylabel("Z Cordinates (arsec)")
+title("X-Z Motion of Dot")
 
 %Image Distortion Animation
 pixel=0.044; %arcsec, 1 pixel size
-sigma=pixel+pixel*rand(1);%random width of bell from 1 to 2 pixels
-grid_s=80; %50 square pixel grid
-[X,Y]=meshgrid(-grid_s/2*pixel:pixel:grid_s/2*pixel, -grid_s/2*pixel:pixel:grid_s/2*pixel);
+sigma=pixel+0.42*dy/1e6;%random width of bell from 1 to 2 pixels
+grid_s=80; %80 square pixel grid
+[X,Z]=meshgrid(-grid_s/2*pixel:pixel:grid_s/2*pixel, -grid_s/2*pixel:pixel:grid_s/2*pixel);
 A=1; %Amplitude
 B=size(cordx); %Size of reshaped cordx matrix
-filename = '60secTestNoisy.gif';
+s=num2str(exposure);str1="Noisy";
+filename=append(str1," ",s,"second exposure.gif");
 f=figure(2);
 axis tight manual
-Z=zeros(length(X),length(Y),B(2)); %meshgrid zero
+Y=zeros(length(X),length(Z),B(2)); %meshgrid zero
 for i=1:B(2)
     for j=1:B(1)
-        xo=cordx(j,i);yo=cordy(j,i);%amplitude
-        Gaus_2d=A*exp(-(((X-xo).^2)/(2*sigma^2)+((Y-yo).^2)/(2*sigma^2)));
-        Z(:,:,i)=Z(:,:,i)+Gaus_2d;
+        xo=cordx(j,i);zo=cordz(j,i);%amplitude
+        Gaus_2d=A*exp(-(((X-xo).^2)/(2*sigma(i)^2)+((Z-zo).^2)/(2*sigma(i)^2)));
+        Y(:,:,i)=Y(:,:,i)+Gaus_2d;
     end
     %Intesnity-weighted centroid of image
-    centroidx(i)=sum(Z(:,:,i).*X)/sum(Z(:,:,i));
-    centroidy(i)=sum(Z(:,:,i).*Y)/sum(Z(:,:,i));
+    centroidx(i)=sum(Y(:,:,i).*X)/sum(Y(:,:,i));
+    centroidz(i)=sum(Y(:,:,i).*Z)/sum(Y(:,:,i));
     if i==1
-        g=pcolor(X,Y,Z(:,:,i));
+        g=pcolor(X,Z,Y(:,:,i));
         title("Image Animation")
         xlabel("X cord (arcsec)")
-        ylabel("Y cord (arcsec)")
+        ylabel("Z cord (arcsec)")
         set(g, "EdgeColor", "none")
         drawnow();
         frame = getframe(f); 
         im = frame2im(frame); 
         [imind,cm] = rgb2ind(im,256);
         imwrite(imind,cm,filename,'gif', 'Loopcount',inf,'DelayTime',0); 
-    else 
-        set(g, "CData", Z(:,:,i));
+    elseif i<180
+        set(g, "CData", Y(:,:,i));
         set(g, "EdgeColor", "none")
         drawnow();
         frame = getframe(f); 
         im = frame2im(frame); 
         [imind,cm] = rgb2ind(im,256);
         imwrite(imind,cm,filename,'gif','WriteMode','append','DelayTime',0);
+    else
+        continue
     end
 end
-figure()
+figure(3)
 hold on;grid on;
-plot(centroidx,centroidy,"o")
+plot(centroidx,centroidz,"o")
 xlabel("Xcord (arcsec)")
-ylabel("Ycord (arcesec)")
-title("Motion of Image Centroid")
+ylabel("Zcord (arcsec)")
+title("Motion of Image Centroid, Noisy")
+
 %Spot Motion Animation Code
 % h = figure;
 % axis tight manual % this ensures that getframe() returns a consistent size
@@ -112,17 +116,17 @@ title("Motion of Image Centroid")
 
 %%% Plotting Section %%%
 % figure()
-% plot(t_sig,DispX_sig)
+% plot(t_sig,DispXnew)
 % xlabel("Time(sec)")
 % ylabel("Disp (m)")
 % title("Mock Up Signal for X from PSD Integration")
 % figure()
-% plot(t_sig,DispY_sig)
+% plot(t_sig,DispYnew)
 % xlabel("Time(sec)")
 % ylabel("Disp (m)")
 % title("Mock Up Signal for Y from PSD Integration")
 % figure()
-% plot(t_sig,DispZ_sig)
+% plot(t_sig,DispZnew)
 % xlabel("Time(sec)")
 % ylabel("Disp (m)")
 % title("Mock Up Signal for Z from PSD Integration")
